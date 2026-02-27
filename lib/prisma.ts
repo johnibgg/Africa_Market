ba6b4f85d9fcca0d269da267614af13d0f0243e7
johnibgg <jnata313@gmail.com>
2026-02-27 11:36:42 +0100

    Fix: final Prisma 7 config - simplify client and schema

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,4 @@
 import { PrismaClient } from "@prisma/client"
-import { PrismaPg } from "@prisma/adapter-pg"
-import pg from "pg"
 
 const prismaClientSingleton = () => {
     return new PrismaClient()
