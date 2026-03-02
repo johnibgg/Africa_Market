e71cf7cf587c635a95152cc8f8bc418fbc11ff35
johnibgg <jnata313@gmail.com>
2026-03-02 11:13:00 +0100

    Fix profile link 404 in Header and MobileNav

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -128,7 +128,7 @@ export function Header() {
                 </div>
                 <DropdownMenuSeparator />
                 <DropdownMenuItem asChild>
-                  <Link href={`/profile/${user?.id}`} className="flex items-center gap-2">
+                  <Link href="/profile" className="flex items-center gap-2">
                     <User className="h-4 w-4" />
                     {t("nav.profile")}
                   </Link>
